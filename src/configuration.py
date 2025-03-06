c3c56bb251085fc1101ac9d3601c92348e26a101
George Perdrizet <george@perdrizet.org>
2025-03-06 13:19:40 +0000

    Added config file for project global paths.

---
+++ src/configuration.py
@@ -0,0 +1,8 @@
+'''Globals for notebooks and modules.'''
+
+# Data
+DATA_FILE='../data/processed/dataset.pkl'
+
+# Trained models
+DECISION_TREE_MODEL='../models/decision_tree.pkl'
+RANDOM_FOREST_MODEL='../models/random_forest.pkl'
